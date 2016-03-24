f6c115bd3bab502242ea8849960dea113a9a1e49
Kevin Bell <kevin.a.bell@gmail.com>
2016-03-24 14:26:17 -0700

    More robust string comparison

--- hello/hello_test.py
+++ hello/hello_test.py
@@ -3,10 +3,16 @@ import unittest
 from .hello_app import app
 
 
-class HelloTest(unittest.TestCase):
+class MyTestCase(unittest.TestCase):
+
+    def assert_contains(self, substring, string):
+        assert substring.upper() in string.upper()
+
+
+class HelloTest(MyTestCase):
 
     def setUp(self):
         self.app = app.test_client()
 
     def test_root(self):
-        assert 'Hello' in self.app.get('/').data
+        self.assert_contains('Hello', self.app.get('/').data)
